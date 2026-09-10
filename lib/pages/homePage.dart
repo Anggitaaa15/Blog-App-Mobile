69f8f71d4c1857b3fa502108994932e21b5d709c
Anggitaaa15 <gitarahayu890@gmail.com>
2026-09-10 23:15:43 +0700

    add branch feature/home

---
+++ lib/pages/homePage.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lintas Kata",
+          style: TextStyle(
+
+          ),
+        ),
+        actions: [
+          IconButton(onPressed: () {}, 
+          icon: Icon(Icons.search),
+          ),
+
+          IconButton(onPressed: () {}, 
+          icon: Icon(Icons.person),
+          )
+        ],
+
+      ),
+    );
+  }
+}
